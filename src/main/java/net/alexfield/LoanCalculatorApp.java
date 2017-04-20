2da522861eb10c8614cd0d9e0fc3f5e80e3f80fe
Alex Field <devmagister@gmail.com>
2017-04-20 16:11:46 -0400

    Initial Commit

---
+++ src/main/java/net/alexfield/LoanCalculatorApp.java
@@ -0,0 +1,12 @@
+package net.alexfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanCalculatorApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanCalculatorApp.class, args);
+    }
+}
