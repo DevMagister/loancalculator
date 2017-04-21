f7eebe1a2571765c83f5bc060911a349d4ab0d49
Alex Field <devmagister@gmail.com>
2017-04-20 20:06:56 -0400

    - More Java Doc

--- src/main/java/net/alexfield/loancalculator/calculators/AutoLoanCalculator.java
+++ src/main/java/net/alexfield/loancalculator/calculators/AutoLoanCalculator.java
@@ -5,10 +5,19 @@ import net.alexfield.loancalculator.api.LoanTerms;
 
 import java.math.BigDecimal;
 
+/**
+ * This calculator will calculate the loan details for Auto Loans which contain a 1% processing fee each month
+ */
 public class AutoLoanCalculator extends AbstractLoanCalculator {
 
+    /** Processing Fee Percentage */
     private static final BigDecimal FEE_PERCENTAGE = new BigDecimal(.01);
 
+    /**
+     * Create an new Auto Loan Calculator with the given terms to be used for Loan Detail calculations
+     *
+     * @param loanTerms loan terms to use for monthly calculations
+     */
     public AutoLoanCalculator(LoanTerms loanTerms) {
         super(loanTerms.getNumMonths(), loanTerms.getLoanAmount(), loanTerms.getInterest());
     }
@@ -31,6 +40,11 @@ public class AutoLoanCalculator extends AbstractLoanCalculator {
         return new LoanDetails(LoanType.AUTO, monthlyPayment);
     }
 
+    /**
+     * Calculate the Monthly Fee amount base on this loans details
+     *
+     * @return Monthly fee amount in dollars
+     */
     private BigDecimal getMonthlyFee() {
         return getLoanAmount().multiply(FEE_PERCENTAGE)
                 .divide(BigDecimal.valueOf(getNumMonths()), BigDecimal.ROUND_HALF_UP);
