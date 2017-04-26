5a4c6889383d75af485725a1a86b956f379b7ec8
Alex Field <alexander.field@ge.com>
2017-04-26 09:44:32 -0400

    - Add support for Equity Loan Calculation

--- src/main/java/net/alexfield/loancalculator/calculators/EquityLoanCalculator.java
+++ src/main/java/net/alexfield/loancalculator/calculators/EquityLoanCalculator.java
@@ -1,10 +1,15 @@
 package net.alexfield.loancalculator.calculators;
 
+import java.math.BigDecimal;
+
 import net.alexfield.loancalculator.api.LoanDetails;
 import net.alexfield.loancalculator.api.LoanTerms;
 
-public class EquityLoanCalculator extends AbstractLoanCalculator
-{
+public class EquityLoanCalculator extends AbstractLoanCalculator {
+
+    /** Processing Fee Percentage */
+    private static final BigDecimal FEE_PERCENTAGE = new BigDecimal(.02);
+
     public EquityLoanCalculator(LoanTerms loanTerms) {
         super(loanTerms.getNumMonths(), loanTerms.getLoanAmount(), loanTerms.getInterest());
     }
@@ -12,6 +17,23 @@ public class EquityLoanCalculator extends AbstractLoanCalculator
     @Override
     public LoanDetails getLoanDetails()
     {
-        return null;
+        /* Get Base Payment */
+        BigDecimal baseMonthlyPayment = calculateBaseMonthlyPayment();
+
+        /* Calculate Interest Monthly Payment */
+        BigDecimal interestMonthlyPayment = calculateEvenlyDispersedInterestPayments();
+
+        /* Calculate Monthly Fees */
+        BigDecimal monthlyFee = calculateMonthlyFee();
+
+        BigDecimal monthlyPayment = baseMonthlyPayment.add(interestMonthlyPayment).add(monthlyFee);
+
+        return new LoanDetails(LoanType.EQUITY, monthlyPayment);
+    }
+
+    private BigDecimal calculateMonthlyFee()
+    {
+        return getLoanAmount().multiply(FEE_PERCENTAGE)
+                .divide(BigDecimal.valueOf(getNumMonths()), BigDecimal.ROUND_HALF_UP);
     }
 }
